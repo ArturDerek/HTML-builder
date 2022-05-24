65ea43785ab5369735cfddc26c59efb8c3ea6a0a
Артур Деревцов <95027038+ArturDerek@users.noreply.github.com>
2022-05-24 10:59:40 +0300

    fix: some mistakes

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,22 +1,31 @@
 let fs = require('fs');
+let path = require('path');
 const readline = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout
 });
-
-fs.createWriteStream('text.txt');
-
-readline.question('Привет! Для завершения работы используй Ctrl+C или ключевое слово exit. Введи текст файла: ',function add(data){
-  if (data==='exit'){
-    console.error('До встречи!');
-  } else {
-    anOne();
-  }
-  function anOne(){
-    fs.appendFile('text.txt', data, (err) => {
-      if (err) throw err;
-      console.log('Уже добавлено в файл: ' + data + '. Продолжайте дополнять файл: ');
-    });
-    console.log('В файл добавлен текст: ' + data);
-  }
+fs.createWriteStream(path.join(__dirname, './text.txt'));
+const showQuestion = () => {
+  readline.question('Для завершения работы используй Ctrl+C или ключевое слово exit. Введи текст файла: ',function add(data){
+    if (data==='exit'){
+      console.log('До встречи!');
+      readline.close();
+    } else {
+      anOne();
+    }
+    function anOne(){
+      fs.appendFile(path.join(__dirname, './text.txt'), data, (err) => {
+        if (err) throw err;
+        else {
+          console.log('Добавлено в файл: ' + data + '. Продолжайте дополнять файл.');
+          showQuestion();
+        }
+      });
+    }
+  });
+};
+showQuestion();
+readline.on('SIGINT', () => {
+  readline.close();
+  console.log('До скорой встречи!');
 });
