67b58be2189583f61c936603083a2929f6941ea4
Артур Деревцов <95027038+ArturDerek@users.noreply.github.com>
2022-05-24 11:18:23 +0300

    fix: some mistakes

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,7 +1,6 @@
 let fs=require('fs');
 let path=require('path');
-
-fs.readdir(path.resolve( 'secret-folder'), {withFileTypes: true}, (err, files)=> {
+fs.readdir(path.resolve( '03-files-in-folder','secret-folder'), {withFileTypes: true}, (err, files)=> {
   if (err) {
     console.log(err);
   } else {
@@ -12,13 +11,18 @@ fs.readdir(path.resolve( 'secret-folder'), {withFileTypes: true}, (err, files)=>
         objNames[i]=objFiles[i].name;
       }
     }
-    console.log(objNames);
+    // console.log(objNames);
     for(let i = 0; i < objNames.length; i++ ){
       if (typeof objNames[i]==='string'){
-        let arrLink='/'+objNames[i];
-        console.log(path.parse(objNames[i]).name+'-'+path.extname(objNames[i])+'-'+arrLink[i].stat.sizes);
+        let arrLink=[];
+        arrLink[i]= path.join(__dirname,'secret-folder', objNames[i]);
+        arrLink[i]=arrLink[i].slice(44);
+        // console.log(arrLink[i]);
+        fs.stat(arrLink[i], (err, stats) => {
+          if (err) throw err;
+          console.log(path.parse(objNames[i]).name+' - '+path.extname(objNames[i])+' - '+ (stats.size / 1024)+' kB');
+        });
       }
     }
-
   }
 });
